12d993c7bbb6134e749143f4d3b77b6110f958eb
dyscope <dyscope@gmail.com>
2024-08-02 18:22:24 +0300

    Added Anime model

--- amscore/admin.py
+++ amscore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Anime
 
-# Register your models here.
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'started_airing')
--- amscore/models.py
+++ amscore/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Anime(models.Model):
+     title = models.CharField("Title", max_length=100)
+     synopsis = models.TextField("Synopsis")
+     img = models.ImageField("Image", upload_to="image/%Y/%m")
+     started_airing = models.DateField("Started airing")
+     finished_airing = models.DateField("Finished airing")
+     publish_date = models.DateField("Publish date")
+     
+     def __str__(self) -> str:
+          return f"{self.title}, {self.started_airing}"
+
+     class Meta:
+          verbose_name = "Anime"
+          verbose_name_plural = "Animes"
